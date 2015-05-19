bbd2a8fc7a193f2a7b7d4c50747e303f5ccf7452
Kevin Krauss <earlkrauss@gmail.com>
2015-05-19 14:50:53 -0700

    charles mata scrabble pairing

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,10 +1,20 @@
 class PlaysController < ApplicationController
   def index
+    @play = Play.new
+
+    @plays = Play.all
   end
 
   def new
   end
 
   def create
+    @play = Play.create(play_params)
+
+    redirect_to '/plays'
+  end
+
+  def play_params
+    params.require(:play).permit(:word)
   end
 end
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,4 +1,5 @@
 class Play < ActiveRecord::Base
+  validates :word, presence: true, length: { maximum: 7 }
 
   def letter_scores
     {"A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4, "G"=>2, "H"=>4, "I"=>1, "J"=>8,
@@ -6,7 +7,17 @@ class Play < ActiveRecord::Base
      "U"=>1, "V"=>4, "W"=>4, "X"=>8, "Y"=>4, "Z"=>10}
   end
 
-  def score
-    word.upcase.chars.inject(0){|sum, letter| sum + letter_scores[letter]}
+  def score(params = {})
+    result = word.upcase.chars.inject(0){|sum, letter| sum + letter_scores[letter]}
+
+    case params[:word_multiplier]
+    when :double
+      result *= 2
+    when :triple
+      result *= 3
+    else
+      result
+    end
+
   end
 end
